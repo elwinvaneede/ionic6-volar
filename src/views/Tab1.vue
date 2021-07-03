8b5728a34767bb7d0f6d3daf069aea4e058c57b0
Elwin van Eede <elwinvaneede@gmail.com>
2021-07-03 17:39:53 +0200

    init

---
+++ src/views/Tab1.vue
@@ -0,0 +1,58 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        <ion-item>
+          <ion-label>Gender</ion-label>
+          <ion-input :placeholder="value"></ion-input>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from "@ionic/vue";
+
+export default defineComponent({
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+  },
+  setup() {
+    const value = ref("value1");
+    return { value };
+  },
+});
+</script>
